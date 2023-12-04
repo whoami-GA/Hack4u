d4aae8bc250e55f40b84f299da79156ae70dcdf8
whoami-GA <122873067+whoami-GA@users.noreply.github.com>
2023-12-04 23:41:12 +0200

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -95,3 +95,98 @@ print(f"\n-------\n")
 
 class Rectangulo:
 
+    def __init__(self, ancho, alto):
+        self.ancho = ancho
+        self.alto = alto
+
+    @property # con property haces como que el def es una propiedad  del objeto y no hace falta poner () en la llamada print
+    def area(self): # Rectangulo.area(rect1)
+        
+        return self.ancho * self.alto
+        
+    def __str__(self):
+        
+        return f"\n[+] Propriedades del rectangulo: [Ancho: {self.ancho}] [Alto {self.alto}]"
+
+    def __eq__(self, rect2):
+                # slef.ancho(rect1) == self.ancho(rect2) and self.alto(rect1) == self.alto(rect2)
+        return self.ancho == rect2.ancho and self.alto == rect2.alto
+
+
+
+rect1 = Rectangulo(20, 80)
+rect2 = Rectangulo(20, 80)
+print(rect1)
+print(f"\n[+] El area es {rect1.area}")
+print(f"\n[+] Son iguales? -> {rect1 == rect2}")
+print(f"\n-------\n")
+
+
+
+class Libro:
+rect1 = Rectangulo(20, 80)
+rect2 = Rectangulo(20, 80)
+print(rect1)
+print(f"\n[+] El area es {rect1.area}")
+print(f"\n[+] Son iguales? -> {rect1 == rect2}")
+print(f"\n-------\n")
+
+
+
+class Libro:
+
+    IVA = 0.21
+rect1 = Rectangulo(20, 80)
+rect2 = Rectangulo(20, 80)
+print(rect1)
+print(f"\n[+] El area es {rect1.area}")
+print(f"\n[+] Son iguales? -> {rect1 == rect2}")
+rect1 = Rectangulo(20, 80)
+rect2 = Rectangulo(20, 80)
+print(rect1)
+print(f"\n[+] El area es {rect1.area}")
+print(f"\n[+] Son iguales? -> {rect1 == rect2}")
+print(f"\n-------\n")
+
+
+
+class Libro:
+
+    IVA = 0.21
+print(f"\n[+] Son iguales? -> {rect1 == rect2}")
+print(f"\n-------\n")
+
+
+
+class Libro:
+
+    IVA = 0.21
+
+    def __init__(self, titulo, autor, precio):
+
+        self.titulo = titulo
+        self.autor = autor
+        self.precio = precio
+
+    @staticmethod # Decoradores, los cuales como argumentos no necesita el objeto como tal para operar sino que opera con los argumentos
+    def es_bestseller(total_ventas): # Libro.es_bestseller(mi_libro, total_ventas)
+
+        return total_ventas > 8000 
+
+    @classmethod #Decoradores 
+    def precio_con_iva(cls, precio):
+
+        return precio + precio * cls.IVA
+
+class LibroDigital(Libro):
+    
+    IVA = 0.10
+
+
+mi_libro = Libro("Ai 25 de Lei?", "Adrian", 13.5)
+mi_libro_digital = LibroDigital("Ai 15 lei pe card?", "Adrian", 13.5)
+# Esto que le estamos pasando no es un atributo del objeto, no existe en el objeto
+print(Libro.es_bestseller(9000))
+print(f"\n[+]El precio del libro con IVA incluido es de {Libro.precio_con_iva(mi_libro.precio)}")
+print(f"\n[+]El precio del libro digital con IVA incluido es de {LibroDigital.precio_con_iva(mi_libro_digital.precio)}")
+
